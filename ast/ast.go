eac55e1e4408d6237e1819704b35737ccba5a0e4
Jesse Williams <jesse@negativespacesounds.com>
2024-05-19 20:36:45 +1000

    test: operator precedence tests

--- ast/ast.go
+++ ast/ast.go
@@ -146,7 +146,7 @@ func (ie *InfixExpression) String() string {
 	var out bytes.Buffer
 	out.WriteString("(")
 	out.WriteString(ie.Left.String())
-	out.WriteString(ie.Operator)
+	out.WriteString(" " + ie.Operator + " ")
 	out.WriteString(ie.Right.String())
 	out.WriteString(")")
 	return out.String()
